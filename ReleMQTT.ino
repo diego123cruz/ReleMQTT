052edfcf07aec3c65d97399c423a41eaa52f39c8
Diego Cruz <diego.123.cruz@gmail.com>
2018-06-06 22:21:23 -0300

    first commit

---
+++ ReleMQTT.ino
@@ -0,0 +1,22 @@
+/*
+ *  ReleMQTT board - ESP8266
+ *  
+ *  by Diego Cruz
+ *  
+ *  Start in 2018-06-06 22:00
+ *  
+ */
+
+
+ // define pins Esp-01
+#define RELE 2
+#define BUTTON 0
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
+
